bd6d0b89cc15f32c0db12a75a8c86443712a0ad4
Josh Kneeland <josh@joshkneeland.com>
2022-10-28 01:24:06 -0600

    Added responsive design and svg logos to the site

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,14 @@
-import React from "react";
 import Clock from "./components/clock/Clock";
 import "./App.css";
-import LogoImg from './images/dente-tempo-logo-horizontal.svg';
+import LogoDesktop from './images/dente-tempo-logo-horizontal.svg';
+import LogoMobile from './images/dente-tempo-logo-vertical.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        {/* <h1>Dente Tempo</h1> */}
-        <img src={LogoImg} alt='Dente Tempo Logo' className='dente-tempo-logo'/>
+        <img src={LogoDesktop} alt='Dente Tempo Logo' className='logo-desktop'/>
+        <img src={LogoMobile} alt='Dente Tempo Logo' className='logo-mobile'/>
         <Clock/>
       </header>
     </div>
